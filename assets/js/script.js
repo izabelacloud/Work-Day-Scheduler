dd48b83eab1c71bedc6267429cc349d97e84c0b1
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-10 17:33:23 -0700

    initial framework

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// var currentDate = moment();
+// console.log(currentDate);
+// // currentDate.format("MM DD YYYY")
+// var date = moment(currentDate).format("dddd")
+// console.log(date);
+var displayDate = $("#currentDay")
+// displayDate = moment();
+var date = moment(displayDate).format("dddd")
+// console.log(date)
+
+
+
